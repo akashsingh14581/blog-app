97b85b2f0dce905181fecb49edd09a0c73cb2ffd
Akash <yadav.akash007@gmail.com>
2025-08-12 13:03:49 +0530

    Refactor code structure for improved readability and maintainability

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { useContext, useEffect } from "react";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import BlogPage from "./pages/BlogPage";
 import TagPage from "./pages/TagPage";
-import CategoryPage from "./pages/CategoryPage"
+import CategoryPage from "./pages/CategoryPage";
 import { AppContext } from "./context/AppContext";
 import "./App.css";
 import { Routes, Route, useSearchParams, useLocation } from "react-router-dom";
 
 export default function App() {
   const { fetchBlogPosts } = useContext(AppContext);
-    const location = useLocation();
-  
+  const location = useLocation();
+  const [searchParams] = useSearchParams(); // move this above useEffect
+
   useEffect(() => {
     const page = searchParams.get("page") ?? 1;
+
     if (location.pathname.includes("tags")) {
-      // iska mtlb tag wala page show krna h
       const tag = location.pathname.split("/").at(-1).replace("-", " ");
       fetchBlogPosts(Number(page), tag);
     } else if (location.pathname.includes("category")) {
@@ -23,17 +24,14 @@ export default function App() {
     } else {
       fetchBlogPosts(Number(page));
     }
-  }, [location.pathname, location.search]);
-
-  const [searchParams, setSearchParams] = useSearchParams();
-
+  }, [location.pathname, location.search, fetchBlogPosts, searchParams]); // fix dependencies
 
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/blog/:blogId" element={<BlogPage />}></Route>
-      <Route path="/tags/:tag" element={<TagPage />}></Route>
-      <Route path="/category/:category" element={<CategoryPage />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:blogId" element={<BlogPage />} />
+      <Route path="/tags/:tag" element={<TagPage />} />
+      <Route path="/category/:category" element={<CategoryPage />} />
     </Routes>
   );
 }
