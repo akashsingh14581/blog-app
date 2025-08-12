d98b2ae1c45aff3e4b79012b36b52dcabbe12309
Akash <yadav.akash007@gmail.com>
2025-08-12 13:25:25 +0530

    fix: update fetchBlogPosts function to use useCallback and improve dependency management

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,50 +1,42 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useCallback } from "react";
 import { baseUrl } from "../baseUrl";
 import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
-export default function AppContextProvider({children}){
+export default function AppContextProvider({ children }) {
     const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);  // initial post is empty array
     const [page, setPage] = useState(1);    // initial page is 1
     const [totalPages, setTotalPages] = useState(null); // in this, we don't know what may be the initial value so that is why null
     const navigate = useNavigate();
 
+    const fetchBlogPosts = useCallback(async (page = 1, tag = null, category) => {
+        setLoading(true);
+        let url = `${baseUrl}?page=${page}`;
+        if (tag) url += `&tag=${tag}`;
+        if (category) url += `&category=${category}`;
 
- async function fetchBlogPosts(page = 1, tag=null, category) {
-    setLoading(true);
-     let url = `${baseUrl}?page=${page}`
-     if(tag){
-        url += `&tag=${tag}`
-     }
-     if(category){
-        url += `&category=${category}`
-     }
-    try {
-       
-        const result = await fetch(url);
-        const data = await result.json();
-        console.log(data);
-        setPage(data.page);
-        setPosts(data.posts);
-        setTotalPages(data.totalPages);
+        try {
+            const result = await fetch(url);
+            const data = await result.json();
+            setPage(data.page);
+            setPosts(data.posts);
+            setTotalPages(data.totalPages);
+        } catch (error) {
+            console.error("Error fetching posts:", error);
+            setPage(1);
+            setPosts([]);
+            setTotalPages(null);
+        }
 
-    } catch (error) {
-        console.error('error in fetching data', error);
-        setPage(1);
-        setPosts([]);
-        setTotalPages(null);
+        setLoading(false);
+    }, []);
 
+    function handlePageChange(page) {
+        navigate({ search: `?page=${page}` })
+        setPage(page);
     }
-
-    setLoading(false);
- }
-
- function handlePageChange(page){
-    navigate({search: `?page=${page}`})
-    setPage(page);
- }
 
     const value = {
         posts,
